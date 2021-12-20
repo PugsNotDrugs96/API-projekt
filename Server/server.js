4b8d7c080e2eb1fa414751c5c3fd497a3390946c
Sahand <sahand.prs@gmail.com>
2021-12-20 13:36:49 +0100

    Just Testing some servers

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
